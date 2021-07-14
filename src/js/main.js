2147639592129025a776f0e2236f72fd5f56ad97
Borman Karlovich <mrgabrielripper@gmail.com>
2021-07-14 20:59:28 +0600

    Added a popup for images, images have adaptive sizes

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,14 @@ import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
+import images from './modules/images';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     let modalState = {};
+    let deadline = '2021-07-23';
 
     changeModalState(modalState);
     modals();
@@ -15,4 +18,6 @@ window.addEventListener('DOMContentLoaded', () => {
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
     forms(modalState);
+    timer('.container1', deadline);
+    images();
 });
